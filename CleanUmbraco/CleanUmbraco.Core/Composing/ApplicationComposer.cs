1316572cc3f7c0548f36568791be7dd2be747a5d
Bijesh <bijesh.tank@maglabs.net>
2020-08-21 16:54:34 +0100

    Add slack integration part1

---
+++ CleanUmbraco/CleanUmbraco.Core/Composing/ApplicationComposer.cs
@@ -0,0 +1,36 @@
+﻿using SlackBotMessages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Composing;
+using System.Web.Configuration;
+using SlackBotMessages.Models;
+
+namespace CleanUmbraco.Core.Composing
+{
+    public class ApplicationComposer : ComponentComposer<ApplicationComponent>
+    {
+
+    }
+
+    public class ApplicationComponent : IComponent
+    {
+        public void Initialize() 
+        {
+            var client = new SbmClient(WebConfigurationManager.AppSettings["SlackBokMessagesWebHookUrl"]);
+
+            var message = new Message
+            {
+                Username = "Alien",
+                Text = "Hello from an Alien",
+                IconEmoji = Emoji.Alien
+            };
+
+            client.Send(message);
+        }
+
+        public void Terminate() { }
+    }
+}
